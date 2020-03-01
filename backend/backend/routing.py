885f05c37be1299ad73bb6eb238249dc200b1fe7
Bence <btoth0211@gmail.com>
2020-03-01 15:41:54 +0100

    docker with daphne and gunicorn

--- backend/backend/routing.py
+++ backend/backend/routing.py
@@ -1,10 +1,10 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.conf.urls import url
+from django.urls import path
 
 from yeelight_smartlamp.consumer import YeelightConsumer
 
-application = ProtocolTypeRouter({
-    'websocket': URLRouter([
-        url('yeelight', YeelightConsumer)
-    ])
-})
+application = URLRouter([
+    # USE WS/
+    path('ws/yeelight', YeelightConsumer)
+    # USE WS/
+])
