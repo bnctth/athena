260e54861c80bc67fb0385e55c6e40d648a12b1a
Bence <btoth0211@gmail.com>
2020-03-10 18:15:43 +0100

    removed faulty test

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -11,10 +11,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:athena/main.dart';
 
 void main() {
-//  testWidgets('Form generating test', (WidgetTester tester) async {
-//    // Build our app and trigger a frame.
+  testWidgets('Form generating test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
 //    await tester.pumpWidget(MyApp());
 //    expect(find.widgetWithText(TextFormField, 'Hostname'), findsOneWidget);
-//
-//  });
+
+  });
 }
